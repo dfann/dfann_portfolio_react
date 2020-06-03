becfdf331cfc798187372c9190e5b2ed0e020c50
MacBook <macbook@MacBooks-MBP.fios-router.home>
2020-06-03 02:49:40 -0400

    Converted HomeSection to React

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import emailImg from "../images/email-gold.png";
-import githubImg from  "../images/github-gold.png";
-import linkedinImg from  "../images/linkedin-gold.png";
+import githubImg from "../images/github-gold.png";
+import linkedinImg from "../images/linkedin-gold.png";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const HomeSection = () => {
     return (
         <section id="home">
-            <div class="container content home-content">
-                <div class="row">
-                    <div class="col-xs-12 jumbotron">
+            <Jumbotron>
+            <Container className="content home-content">
+                <Row>
+                    <Col xs={12}>
                         <h1>David Fann</h1>
                         <h2>Let's Build Something Great Together</h2>
                         <hr />
-                        <div class="row social-links-row">
-                            <div class="col-md-4">
-                                <a class="btn btn-primary btn-lg social-links" href="mailto:davidthomasfann@gmail.com" role="button">
-                                    <img src={emailImg} />
-                              Contact
-                              </a>
-                            </div>
-                            <div class="col-md-4" >
-                                <a class="btn btn-primary btn-lg social-links" href="https://github.com/dfann" role="button">
-                                    <img src={githubImg} />
-                                Github
-                                </a>
-                            </div>
-                            <div class="col-md-4">
-                                <a class="btn btn-primary btn-lg social-links" href="https://www.linkedin.com/in/dfann" role="button">
-                                    <img src={linkedinImg} />
-                                Linkedin
-                                </a>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+                <Row className="social-links-row">
+                    <Col xs={4}>
+                        <Button href="mailto:davidthomasfann@gmail.com" className="social-links" variant="link">
+                            <img src={emailImg} />
+                            Contact
+                         </Button>
+                         <Button href="https://github.com/dfann" className="social-links" variant="link">
+                        <img src={githubImg} />
+                            Github
+                         </Button>     
+                         <Button href="https://www.linkedin.com/in/dfann" className="social-links" variant="link">
+                         <img src={linkedinImg} />
+                            Linkedin
+                         </Button>     
+                    </Col>
+                </Row>
+            </Container>
+            </Jumbotron>
         </section>
 
     );
